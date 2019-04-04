bdc4e46a28b63a335fd3fb1d8e5310d95d318d88
Shaon Majumder <shaon.fdr@robist.com>
2019-04-05 05:28:11 +0600

    sorting and config

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Youtube Title Grabber
-	Get the output as a text file containing all the video titles with their urls
+# Daily_Food_Requirement
--- youtube_title_grabber.py
+++ youtube_title_grabber.py
@@ -1,6 +1,23 @@
 from bs4 import BeautifulSoup
 from requests.exceptions import ConnectTimeout,ReadTimeout,ConnectionError
-import os,re,requests,time,random,codecs,json,chardet,winsound,shutil
+import os,re,requests,time,random,codecs,json,chardet,winsound,shutil,configparser
+
+utube_title_seperating_flag = ' =u= '
+
+config = configparser.ConfigParser()
+config.readfp(codecs.open("config.ini", "r", "utf8"))
+
+get_utube_title_choice = config['GETTING_UTUBE_VIDEO_TITLE']['GET_TITLE']
+Get_utube_video_title_Input_File_Name = config['GETTING_UTUBE_VIDEO_TITLE']['Input_File_Name']
+Get_utube_video_title_Output_File_Name = config['GETTING_UTUBE_VIDEO_TITLE']['Output_File_Name']
+Get_utube_video_title_Show_Output = config['GETTING_UTUBE_VIDEO_TITLE']['Show_Output']
+
+sort_choice = config['SORTING']['Sort']
+sorting_tag_list = config['SORTING']['Tag_List'].split(',')
+Sorting_Input_File_Name = config['SORTING']['Input_File_Name']
+Sorting_Output_File_Name = config['SORTING']['Output_File_Name']
+Sorting_Show_Output = config['SORTING']['Show_Output']
+
 
 def read_file(filename):
 	with codecs.open(filename, "r", encoding="utf-8") as file_reader:
@@ -47,7 +64,48 @@ def get_site_title(url):
 	YouTube_title = page.title.text.replace(" - YouTube","")
 	return YouTube_title
 
-lines = read_file('YouTube.txt')
-title_urls = '\n'.join([get_site_title(url) + ' - ' + url for url in lines])
-write_file('YouTube_.txt', title_urls,mode="w")
-print(title_urls)
+
+
+def list_utbube_title(filename):
+	lines = read_file(filename)
+	return [get_site_title(url) + utube_title_seperating_flag + url for url in lines]	
+
+def find_tag_in_list(target_li,tag):
+	return [stri for stri in target_li if tag in stri]
+
+
+def sort_utube_urls_by_tag(filename,tag_list):
+	titles = read_file(filename)
+	sorted_li_1 = []
+	sorted_li_2 = []
+	sorted_li = []
+	for tag in tag_list:
+		for stri in titles:
+			stri_ = stri.split(utube_title_seperating_flag)[0]
+			if tag in stri_:
+				sorted_li_1.append(stri)
+			else:
+				sorted_li_2.append(stri)
+		sorted_li = sorted_li + sorted_li_1
+		titles = sorted_li_2
+		sorted_li_1 = []
+		sorted_li_2 = []
+
+	return sorted_li + titles
+
+
+if(get_utube_title_choice == 'Yes'):
+	title_urls = list_utbube_title(Get_utube_video_title_Input_File_Name)
+	if(get_utube_title_choice == 'Yes'):
+		print(title_urls)
+	title_urls = '\n'.join(title_urls)
+	write_file(Sorting_Output_File_Name, title_urls,mode="w")
+	
+
+if(sort_choice == 'Yes'):
+	sorted_title_urls = sort_utube_urls_by_tag(Sorting_Input_File_Name,sorting_tag_list)
+	if(Sorting_Show_Output == 'Yes'):
+		print(sorted_title_urls)
+	sorted_title_urls ='\n'.join(sorted_title_urls)
+	write_file(Sorting_Output_File_Name, sorted_title_urls,mode="w")
+
